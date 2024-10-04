432194483ccf3d725ac6781569ca7b642d92c8c9
Mohammed-nasr7 <m.n.elfeky@gmail.com>
2024-10-04 19:47:48 +0300

    Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,40 +1,34 @@
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  
-CELSIUS_TO_FAHRENHEIT_OFFSET = 32     
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
 
 def convert_to_celsius(fahrenheit):
-  
-    celsius = (fahrenheit - CELSIUS_TO_FAHRENHEIT_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
-    return celsius
+    
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 def convert_to_fahrenheit(celsius):
- 
-    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + CELSIUS_TO_FAHRENHEIT_OFFSET
-    return fahrenheit
+    
+    return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
 def main():
-   
     try:
        
         temperature = float(input("Enter the temperature to convert: "))
+        unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+
+        if unit == 'C':
+           
+            converted = convert_to_fahrenheit(temperature)
+            print(f"{temperature}°C is {converted}°F")
+        elif unit == 'F':
+           
+            converted = convert_to_celsius(temperature)
+            print(f"{temperature}°F is {converted}°C")
+        else:
+            print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+
     except ValueError:
-        raise ValueError("Invalid temperature. Please enter a numeric value.")
-
-   
-    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
-
-    if unit == 'C':
-       
-        converted_temp = convert_to_fahrenheit(temperature)
-        print(f"{temperature}°C is {converted_temp}°F")
-
-    elif unit == 'F':
-        
-        converted_temp = convert_to_celsius(temperature)
-        print(f"{temperature}°F is {converted_temp}°C")
-
-    else:
-        print("Invalid unit. Please enter 'C' for Celsius or 'F' for Fahrenheit.")
+        print("Invalid temperature. Please enter a numeric value.")
 
 if __name__ == "__main__":
     main()
