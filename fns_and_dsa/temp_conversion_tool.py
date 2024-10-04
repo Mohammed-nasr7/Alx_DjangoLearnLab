e77a3520f6a8983726697fc6fcc78a5bcaeea106
Mohammed-nasr7 <m.n.elfeky@gmail.com>
2024-10-04 17:42:26 +0300

    Temperature Conversion Tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,24 +1,23 @@
 
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9  
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5 
-CELSIUS_TO_FAHRENHEIT_OFFSET = 32    
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5  
+CELSIUS_TO_FAHRENHEIT_OFFSET = 32      
 
 def convert_to_celsius(fahrenheit):
-  
-   
+    
     celsius = (fahrenheit - CELSIUS_TO_FAHRENHEIT_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
 
 def convert_to_fahrenheit(celsius):
-   
+    
    
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + CELSIUS_TO_FAHRENHEIT_OFFSET
     return fahrenheit
 
 def main():
-    
+   
     try:
-      
+       
         temperature = float(input("Enter the temperature to convert: "))
     except ValueError:
         raise ValueError("Invalid temperature. Please enter a numeric value.")
@@ -28,12 +27,12 @@ def main():
 
     if unit == 'C':
        
-        converted_temp = (temperature * CELSIUS_TO_FAHRENHEIT_FACTOR) + CELSIUS_TO_FAHRENHEIT_OFFSET
+        converted_temp = convert_to_fahrenheit(temperature)
         print(f"{temperature}°C is {converted_temp}°F")
 
     elif unit == 'F':
-       
-        converted_temp = (temperature - CELSIUS_TO_FAHRENHEIT_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
+      
+        converted_temp = convert_to_celsius(temperature)
         print(f"{temperature}°F is {converted_temp}°C")
 
     else:
