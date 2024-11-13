e8b7cc550faa3ed460f8e54fb2fafe48872e0062
mohammed nasr <m.n.elfeky@gmail.com>
2024-11-13 17:26:35 +0200

    first commit

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,35 +1,16 @@
 from relationship_app.models import Author, Book, Library, Librarian
 
-
-def books_by_author(author_name):
+def query_books_by_author(author_name):
     author = Author.objects.get(name=author_name)
-    books = author.books.all()
-    for book in books:
-        print(f'Title: {book.title}, Author: {book.author.name}')
+    books = Book.objects.filter(author=author)
+    return books
 
-
-def books_in_library(library_name):
+def list_books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     books = library.books.all()
-    for book in books:
-        print(f'Title: {book.title}, Library: {library.name}')
+    return books
 
-
-def librarian_for_library(library_name):
+def retrieve_librarian_for_library(library_name):
     library = Library.objects.get(name=library_name)
-    librarian = library.librarian
-    print(f'Librarian: {librarian.name}, Library: {library.name}')
-
-
-if __name__ == '__main__':
-   
-    print("Books by George Orwell:")
-    books_by_author('George Orwell')
-
-  
-    print("\nBooks in City Library:")
-    books_in_library('City Library')
-
-   
-    print("\nLibrarian for City Library:")
-    librarian_for_library('City Library')
+    librarian = Librarian.objects.get(library=library)
+    return librarian
