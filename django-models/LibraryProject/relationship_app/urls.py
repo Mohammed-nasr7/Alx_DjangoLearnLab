f42b91190846fbf689329c11713e484b7235cbe9
mohammed nasr <m.n.elfeky@gmail.com>
2024-11-16 18:19:48 +0200

    commit

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,13 +1,18 @@
+# relationship_app/urls.py
+
 from django.urls import path
-from . import views
-from django.urls import path
-from . import views
+from .views import list_books, LibraryDetailView, register, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-]
 
-urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+    path('book/', views.list_books, name='book_list'),
+    path('add_book/', views.add_book, name='add_book'),  
+    path('edit_book/', views.edit_book, name='edit_book'),  
+    path('book/<int:pk>/delete/', views.delete_book, name='delete_book'),
+
+    
+    path('books/', list_books, name='list_books'),  # URL for listing books in plain text
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),  # URL for library detail view
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('register/', views.register, name='register'),
 ]
