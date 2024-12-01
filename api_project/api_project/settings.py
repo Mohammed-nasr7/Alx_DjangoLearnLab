ea1c4e727f8696a37d5d0a5f461de7347f7cc17a
mohammed nasr <m.n.elfeky@gmail.com>
2024-12-01 22:25:19 +0200

    commit

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -123,10 +123,4 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.permissions.IsAuthenticated'
-    ),
-}
+pip install djangorestframework-authtoken
