deadca0fa5f9d5bf9568f9cc9b04a7f70b1e69ff
mohammed nasr <m.n.elfeky@gmail.com>
2024-12-22 19:44:32 +0200

    commit

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,14 +1,20 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
-from accounts.models import CustomUser
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model, authenticate
+
+# استيراد النموذج المخصص للمستخدم
+CustomUser = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    # تعريف الحقول التي سيتم استخدامها في النموذج
     class Meta:
         model = CustomUser
         fields = ('username', 'password', 'email', 'bio', 'profile_picture')
 
+    # تأكد من أن كلمة المرور مشفرة
     def create(self, validated_data):
-        user = CustomUser.objects.create_user(**validated_data)
+        user = CustomUser.objects.create_user(**validated_data)  # استخدام create_user لإنشاء مستخدم آمن
+        Token.objects.create(user=user)  # إنشاء توكن مرتبط بالمستخدم
         return user
 
 class LoginSerializer(serializers.Serializer):
@@ -16,6 +22,7 @@ class LoginSerializer(serializers.Serializer):
     password = serializers.CharField()
 
     def validate(self, data):
+        # التحقق من صحة اسم المستخدم وكلمة المرور
         user = authenticate(username=data['username'], password=data['password'])
         if user is None:
             raise serializers.ValidationError('Invalid credentials')
